51af4f3a88cdeb97e9d6b7eb73f0f185ac10981b
alienwu2018 <582946945@qq.com>
2023-04-02 14:47:50 +0800

    test v6

--- module/src/main/cpp/hack.cpp
+++ module/src/main/cpp/hack.cpp
@@ -24,8 +24,8 @@ static int GetAndroidApiLevel() {
 }
 
 void hack_start(const char *game_data_dir) {
-    void* h = dlopen("/data/local/tmp/libTdj.so", 0);
-    LOGI(">>>my so handle:0x%lx",h);
+    dlopen("/data/local/tmp/libTdj.so", 0);
+    LOGI(">>>my so handle");
     bool load = false;
     for (int i = 0; i < 10; i++) {
         void *handle = xdl_open("libil2cpp.so", 0);
